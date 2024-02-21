ba38755f4c6d20825fad4bb91c35b249668f0a65
kseysh <kseysh123@naver.com>
2024-02-22 00:03:23 +0900

    feat - health check api 작성

--- util/views.py
+++ util/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def health(request):
+    return HttpResponse(status = 200, content = "seminar server ok!")
