d4bf26f23bf7a84940d999360d1f16d7d5280362
kseysh <kseysh123@naver.com>
2024-02-22 00:10:08 +0900

    feat - 공통 url과 lionapp url 연결

--- seminar_project/seminar_project/urls.py
+++ seminar_project/seminar_project/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('health/', include('util.urls')),
+    path('api/', include('lionapp.urls')), 
 ]
