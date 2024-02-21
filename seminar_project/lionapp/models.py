65c2ddfa7274a612276e3cbca7ccc1fb387a7ae6
kseysh <kseysh123@naver.com>
2024-02-22 00:11:57 +0900

    feat - lionapp model 생성

--- seminar_project/lionapp/models.py
+++ seminar_project/lionapp/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django.urls import path
 
-# Create your models here.
+
+from lionapp import views
+
+urlpatterns = [
+    path('create/',views.create_post),
+    path('<int:pk>/',views.get_post),
+    path('delete/<int:pk>',views.delete_post)
+]
