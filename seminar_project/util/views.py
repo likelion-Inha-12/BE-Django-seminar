5242d7ccab08fbd5ba752aca77dc4d12df4abc07
kseysh <kseysh123@naver.com>
2024-03-03 03:45:35 +0900

    feat: user login signup 기능 구현

--- seminar_project/util/views.py
+++ seminar_project/util/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 from rest_framework.response import Response
-
-
 
 def health(request):
     return HttpResponse(status = 200, content = "seminar server ok!")
